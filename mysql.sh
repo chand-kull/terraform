0c6a257323030547ca8e073eb402ff0a41acb5e0
chandugit config --global user.email cheerlachandrasekhar81@gmail.com <syedfarzana589@gmail.com>
2025-11-08 23:30:50 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+USERID=$(id -u) 
+TIMESTAMP=$(date +%F-%H-%M-%S) # to check time
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #script name
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log #to save script name with time
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then 
+      echo "$2..FAILURE"
+      exit 1
+   else
+    echo "$2 ...SUCCESS"
+   fi
+}
+
+if [ $USERID  -ne 0 ]
+then 
+ echo " please run this script with super user"
+ exit 1 
+else 
+ echo"  you are a super user"
+fi
+
+dnf install mysql-server -y &>>$LOGFILE
+validate $? "installing mysql-server"
+
+
+
+ 
